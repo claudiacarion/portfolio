27a05d2732b28774638fcd5739c8428bd3da0bd2
claudiacarion <claudia.carion@gmail.com>
2026-01-12 13:35:47 +0100

    add fetch

--- scripts/connect.js
+++ scripts/connect.js
@@ -1,76 +1,75 @@
 const connect = () => {
   main.innerHTML = `
-        <h1 class="page-title">Let's connect!</h1>
-      <section class="contact-container">
-        <div class="contact">
-          <div class="method">
-            <i class="fa-solid fa-envelope"></i>
-            <form
-              action="mailto: claudia.carion@gmail.com"
-              method="POST"
-              enctype="text/plain"
-              class="user-message"
-            >
-              <fieldset class="email">
-                <legend>Leave me a message</legend>
-                <div class="user-input">
-                  <label for="name">Name* </label>
-                  <input
-                    type="text"
-                    name="name"
-                    id="name"
-                    placeholder="Enter your name"
-                    class="text"
-                    required
-                  />
-                </div>
-                <div class="user-input">
-                  <label for="email">Email* </label>
-                  <input
-                    type="email"
-                    name="email"
-                    id="email"
-                    placeholder="Enter your email"
-                    class="text"
-                    required
-                  />
-                </div>
-                <div class="user-input">
-                  <label for="message">Message*</label>
-                  <textarea
-                    name="message"
-                    id="message"
-                    placeholder="Enter your message"
-                    class="message-area"
-                    required
-                  ></textarea>
-                </div>
-                <button class="submit">Send</button>
-              </fieldset>
-            </form>
+  <h1 class="page-title">Let's connect!</h1>
+  <section class="contact-container">
+  <div class="contact">
+    <div class="method">
+      <i class="fa-solid fa-envelope"></i>
+      <form
+        action="mailto: claudia.carion@gmail.com"
+        method="POST"
+        enctype="text/plain"
+        class="user-message"
+      >
+        <fieldset class="email">
+          <legend>Leave me a message</legend>
+          <div class="user-input">
+            <label for="name">Name* </label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter your name"
+              class="text"
+              required
+            />
           </div>
-          <div class="method">
-            <a
-              href="https://www.linkedin.com/in/claudia-pereira-carion/"
-              target="_blank"
-              class="link"
-            >
-              <i class="fa-brands fa-linkedin"></i>
-            </a>
-            <p>Connect on LinkedIn</p>
+          <div class="user-input">
+            <label for="email">Email* </label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email"
+              class="text"
+              required
+            />
           </div>
-                    <div class="method">
-            <a
-              href="https://github.com/claudiacarion"
-              target="_blank"
-              class="link"
-            >
-              <i class="fa-brands fa-square-github"></i
-            ></a>
-            <p>Check out my Github</p>
+          <div class="user-input">
+            <label for="message">Message*</label>
+            <textarea
+              name="message"
+              id="message"
+              placeholder="Enter your message"
+              class="message-area"
+              required
+            ></textarea>
           </div>
-        </div>
-
+          <button class="submit">Send</button>
+        </fieldset>
+      </form>
+    </div>
+    <div class="method">
+      <a
+        href="https://www.linkedin.com/in/claudia-pereira-carion/"
+        target="_blank"
+        class="link"
+      >
+        <i class="fa-brands fa-linkedin"></i>
+      </a>
+      <p>Connect on LinkedIn</p>
+    </div>
+              <div class="method">
+      <a
+        href="https://github.com/claudiacarion"
+        target="_blank"
+        class="link"
+      >
+        <i class="fa-brands fa-square-github"></i
+      ></a>
+      <p>Check out my Github</p>
+    </div>
+  </div>
 `;
 };
 
--- scripts/playground.js
+++ scripts/playground.js
@@ -11,26 +11,100 @@ const projects = () => {
     });
 };
 
-const playground = projects => {
-  main.innerHTML = `
-  <h1 class="main__title">My Projects</h1>
-  <ul class="list" id="projects"></ul>
-  `;
-
+const github = projects => {
   projects.forEach(project => {
-    const li = document.createElement("li");
-    li.textContent = project.name;
-    document.querySelector("#projects").appendChild(li);
+    const name = document.createElement("p");
+    name.innerHTML = `
+    <a href=${project.html_url}>${project.name.charAt(0).toUpperCase() + project.name.slice(1).replace("-", " ")}</a>`;
+    document.querySelector(".list").appendChild(name);
   });
 };
 
-let playgroundBtn = document.querySelector("#playground-btn");
-playgroundBtn.onclick = () => {
+const playground = () => {
+  main.innerHTML = `
+  <h1 class="main__title">My Projects</h1>
+    <div class="gallery">
+      <div class="project">
+        <a href="https://dashing-taffy-506794.netlify.app/" target="_blank">
+          <img
+            src="./assets/images/popin.png"
+            alt="popin"
+            height="auto"
+            width="300px"
+            class="project-img"
+        /></a>
+        <h4 class="project-name">Pop In</h4>
+        <p class="description">
+          Final group project at School of Applied Technology, fullstack
+          attendance marking app created with Nextjs, Nextauth and MongoDB,
+          styled with CSS
+        </p>
+      </div>
+      <div class="project">
+        <a href="https://the-friendly-lexicon.netlify.app/" target="_blank"
+          ><img
+            src="./assets/images/lexicon.png"
+            alt="the friendly lexicon"
+            height="auto"
+            width="300px"
+            class="project-img"
+        /></a>
+        <h4 class="project-name">The Friendly Lexicon</h4>
+        <p class="description">
+          Frontend dictionary project created with React and Typescript, styled with
+          Tailwind
+        </p>
+      </div>
+      <div class="project">
+        <a
+          href="https://github.com/claudiacarion/goldenmemeretriever"
+          target="_blank"
+        >
+          <img
+            src="./assets/images/meme.png"
+            alt="meme"
+            height="auto"
+            width="300px"
+            class="project-img"
+        /></a>
+        <h4 class="project-name">Golden (Meme) Retriever</h4>
+        <p class="description">
+          My first fullstack project, a random meme generator, created with React and Express, styled
+          with CSS
+        </p>
+      </div>
+      <div class="project">
+        <a href="https://sequence-memory-challenge.vercel.app/" target="_blank">
+          <img
+            src="./assets/images/memory.png"
+            alt="sequence memory game"
+            height="auto"
+            width="300px"
+            class="project-img"
+        /></a>
+        <h4 class="project-name">Sequence Memory Game</h4>
+        <p class="description">
+          Frontend game project created with HTML and Javascript, styled with CSS
+        </p>
+      </div>
+    </div>
+  <h3 class="project__subtitle" id="other-projects">Other Projects:</h3>
+  <ul class="list hide"></ul>
+  `;
+
   projects()
     .then(data => {
-      playground(data);
+      github(data);
     })
     .catch(error => {
       console.error("Error: ", error);
     });
+
+  const others = document.querySelector("#other-projects");
+  const list = document.querySelector(".list");
+
+  others.onclick = () => list.classList.toggle("hide");
 };
+
+const playgroundBtn = document.querySelector("#playground-btn");
+playgroundBtn.onclick = () => playground();
