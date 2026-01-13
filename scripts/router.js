810ab444dca45109e12cdb54f5d8e2b9d34ef01c
claudiacarion <claudia.carion@gmail.com>
2026-01-13 15:37:11 +0100

    add router function

---
+++ scripts/router.js
@@ -0,0 +1,39 @@
+const main = document.querySelector(".main-container");
+
+const routes = {
+  home: {
+    render: home,
+  },
+  story: {
+    render: story,
+  },
+  playground: {
+    render: playground,
+    init: initialize,
+  },
+  connect: {
+    render: connect,
+  },
+};
+
+const navigate = page => {
+  const route = routes[page];
+  if (!route) return;
+
+  $(".main-container").fadeOut(400, () => {
+    main.innerHTML = route.render();
+
+    if (route.init) {
+      route.init();
+    }
+
+    $(".main-container").fadeIn(400);
+  });
+};
+
+document.querySelector("#home-btn").onclick = () => navigate("home");
+document.querySelector("#story-btn").onclick = () => navigate("story");
+document.querySelector("#playground-btn").onclick = () => navigate("playground");
+document.querySelector("#connect-btn").onclick = () => navigate("connect");
+
+navigate("home");
