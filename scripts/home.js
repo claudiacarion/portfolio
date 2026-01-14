f06aa63e0212c3ff3ef9181d2a5e9cb78da57761
claudiacarion <claudia.carion@gmail.com>
2026-01-14 19:13:06 +0100

    change homepage font size and add footer class

--- scripts/home.js
+++ scripts/home.js
@@ -2,8 +2,8 @@ const home = () => {
   return `
       <div class="home">
         <img src="./assets/images/pfp.webp" alt="profile picture" height="auto" width="300" class="profile-img" />
-        <h1 class="main__title">Hi! I'm Claudia</h1>
-        <h2 class="home__subtitle">Frontend Developer</h2>
+        <h1 class="main__title">Hi! I'm Claudia Pereira Carion</h1>
+        <p class="home__subtitle">JavaScript Developer</p>
       </div>
   `;
 };
