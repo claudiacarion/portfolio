3bfa21d58bc2d167799a1d32ef11dc95a6ac0a59
claudiacarion <claudia.carion@gmail.com>
2026-01-15 18:23:25 +0100

    change pfp height and add defer to jquery

--- scripts/home.js
+++ scripts/home.js
@@ -1,7 +1,7 @@
 const home = () => {
   return `
       <div class="home">
-        <img src="./assets/images/pfp.webp" alt="profile picture" height="auto" width="300" class="profile-img" />
+        <img src="./assets/images/pfp.webp" alt="profile picture" height="400" width="300" class="profile-img" />
         <h1 class="main__title">Hi! I'm Claudia Pereira Carion</h1>
         <p class="home__subtitle">JavaScript Developer</p>
       </div>
