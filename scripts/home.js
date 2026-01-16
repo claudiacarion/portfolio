400c0341de33407e3dae0d1ce94a50b6290720bd
claudiacarion <claudia.carion@gmail.com>
2026-01-16 14:34:27 +0100

    remove script.js

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,10 @@
+const hamburger = document.querySelector(".logo-img");
+const navbar = document.querySelector(".navbar");
+
+hamburger.onclick = () => {
+  navbar.classList.toggle("active")
+}
+
 const home = () => {
   return `
       <div class="home">
--- scripts/script.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector(".logo-img");
-const navbar = document.querySelector(".navbar");
-
-hamburger.onclick = () => {
-  navbar.classList.toggle("active")
-}
