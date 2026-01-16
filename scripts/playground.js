a50777284ff39f4152038213e04f23cc6baa7df7
claudiacarion <claudia.carion@gmail.com>
2026-01-16 12:02:02 +0100

    refactor

--- scripts/playground.js
+++ scripts/playground.js
@@ -28,105 +28,49 @@ const playground = () => {
   return `
   <h1 class="main__title">My Playground</h1>
   <div class="gallery">
-  <a
-    href="https://github.com/claudiacarion/goldenmemeretriever"
-    target="_blank"
-  >
-    <div class="project-card">
-      <h3 class="project-name">Golden (Meme) Retriever</h3>
-        <img
-          src="./assets/images/meme.webp"
-          alt="golden meme retriever"
-          height="auto"
-          width="300px"
-          class="project-img"
-      />
-      <p class="description">
-        My first ever fullstack project, a random meme generator, created with React and Express, styled
-        with CSS
-      </p>
-    </div>
+    <a href="https://github.com/claudiacarion/goldenmemeretriever" target="_blank">
+      <div class="project-card">
+        <h3 class="project-name">Golden (Meme) Retriever</h3>
+        <img src="./assets/images/meme.webp" alt="golden meme retriever" height="auto" width="300px" class="project-img" />
+        <p class="description"> My first ever fullstack project, a random meme generator, created with React and Express, styled with CSS</p>
+      </div>
     </a>
     <a href="https://dashing-taffy-506794.netlify.app/" target="_blank">
-    <div class="project-card">
-      <h3 class="project-name">Pop In</h3>
-          <img
-            src="./assets/images/popin.webp"
-            alt="popin attendance app"
-            height="auto"
-            width="300px"
-            class="project-img"
-        />
-        <p class="description">
-          Final group project at School of Applied Technology, fullstack
-          attendance marking app created with Nextjs, Nextauth and MongoDB,
-          styled with CSS
-        </p>
+      <div class="project-card">
+        <h3 class="project-name">Pop In</h3>
+        <img src="./assets/images/popin.webp" alt="popin attendance app" height="auto" width="300px" class="project-img" />
+        <p class="description">Final group project at School of Applied Technology, fullstack attendance marking app created with Nextjs, Nextauth and MongoDB, styled with CSS</p>
       </div>
-      </a>
-      <a href="https://the-friendly-lexicon.netlify.app/" target="_blank"
-        >
+    </a>
+    <a href="https://the-friendly-lexicon.netlify.app/" target="_blank">
       <div class="project-card">
         <h3 class="project-name">The Friendly Lexicon</h3>
-          <img
-            src="./assets/images/lexicon.webp"
-            alt="the friendly lexicon"
-            height="auto"
-            width="300px"
-            class="project-img"
-        />
-        <p class="description">
-          Frontend dictionary project created with React and Typescript, styled with
-          Tailwind
-        </p>
+        <img src="./assets/images/lexicon.webp" alt="the friendly lexicon" height="auto" width="300px" class="project-img" />
+        <p class="description">Frontend dictionary project created with React and Typescript, styled with Tailwind</p>
       </div>
-      </a>
-      <a href="https://sequence-memory-challenge.vercel.app/" target="_blank">
+    </a>
+    <a href="https://sequence-memory-challenge.vercel.app/" target="_blank">
       <div class="project-card">
         <h3 class="project-name">Sequence Memory Game</h3>
-          <img
-            src="./assets/images/memory.webp"
-            alt="sequence memory game"
-            height="auto"
-            width="300px"
-            class="project-img"
-        />
-        <p class="description">
-          Frontend game created with HTML and Javascript, styled with CSS
-        </p>
+        <img src="./assets/images/memory.webp" alt="sequence memory game" height="auto" width="300px" class="project-img" />
+        <p class="description">Frontend game created with HTML and Javascript, styled with CSS</p>
       </div>
-      </a>
-      <a href="https://australia-zoo-pi.vercel.app/" target="_blank">
+    </a>
+    <a href="https://australia-zoo-pi.vercel.app/" target="_blank">
       <div class="project-card">
         <h3 class="project-name">Aussie Zoo</h3>
-          <img
-            src="./assets/images/australiazoo.webp"
-            alt="aussie zoo website"
-            height="auto"
-            width="300px"
-            class="project-img"
-        />
-        <p class="description">
-          Frontend group project built HTML and Javascript, styled with CSS, focusing on JS objects and Agile methodology
-        </p>
+        <img src="./assets/images/australiazoo.webp" alt="aussie zoo website" height="auto" width="300px" class="project-img" />
+        <p class="description">Frontend group project built HTML and Javascript, styled with CSS, focusing on JS objects and Agile methodology</p>
       </div>
-      </a>
-      <a href="https://api-group-assignment.vercel.app/" target="_blank">
+    </a>
+    <a href="https://api-group-assignment.vercel.app/" target="_blank">
       <div class="project-card">
         <h3 class="project-name">Flick & Feast Roulette</h3>
-          <img
-            src="./assets/images/movienight.webp"
-            alt="flick and feast roulette"
-            height="auto"
-            width="300px"
-            class="project-img"
-        />
-        <p class="description">
-          Frontend group project built with HTML and Javascript, styled with CSS, focusing on fetching from external APIs
-        </p>
+        <img src="./assets/images/movienight.webp" alt="flick and feast roulette" height="auto" width="300px" class="project-img" />
+        <p class="description">Frontend group project built with HTML and Javascript, styled with CSS, focusing on fetching from external APIs</p>
       </div>
-      </a>
-    </div>
+    </a>
+  </div>
   <h3 class="project__subtitle" id="other-projects">See all my repos <i class="fa-solid fa-caret-down"></i></h3>
   <ul class="project-ul hide"></ul>
   `;
