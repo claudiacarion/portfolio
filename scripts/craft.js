969b78af25ceba377872ccdca896b3f8237911a4
claudiacarion <claudia.carion@gmail.com>
2026-01-16 14:41:07 +0100

    refactor craft

--- scripts/craft.js
+++ scripts/craft.js
@@ -4,29 +4,29 @@ const craft = () => {
   <div class="tech-stack">
     <div class="tech-card">
       <h3 class="tech-area">Languages</h3>
-        <p class="tech-name">JavaScript</p>
+      <p class="tech-name">JavaScript</p>
     </div>
     <div class="tech-card">
       <h3 class="tech-area">Frontend</h3>
-        <p class="tech-name">HTML</p>
-        <p class="tech-name">CSS</p>
-        <p class="tech-name">React</p>
-        <p class="tech-name">Tailwind</p>
+      <p class="tech-name">HTML</p>
+      <p class="tech-name">CSS</p>
+      <p class="tech-name">React</p>
+      <p class="tech-name">Tailwind</p>
     </div>
     <div class="tech-card">
       <h3 class="tech-area">Backend</h3>
-        <p class="tech-name">NodeJS</p>
-        <p class="tech-name">MongoDB</p>
-        <p class="tech-name">REST</p>
+      <p class="tech-name">NodeJS</p>
+      <p class="tech-name">MongoDB</p>
+      <p class="tech-name">REST</p>
     </div>
     <div class="tech-card">
       <h3 class="tech-area">Tools</h3>
-        <p class="tech-name">Visual Studio Code</p>
-        <p class="tech-name">Git & Github</p>
-        <p class="tech-name">Netlify</p>
-        <p class="tech-name">Vercel</p>
-        <p class="tech-name">TDD</p>
-        <p class="tech-name">Agile</p>
+      <p class="tech-name">Visual Studio Code</p>
+      <p class="tech-name">Git & Github</p>
+      <p class="tech-name">Netlify</p>
+      <p class="tech-name">Vercel</p>
+      <p class="tech-name">TDD</p>
+      <p class="tech-name">Agile</p>
     </div>
   </div>
   `;
